e04274665ec8be29a4424b94cfb102a2bfcc03e0
Jon George <23463258+JonGeorge@users.noreply.github.com>
2020-10-29 17:16:49 -0400

    Set up initial React app

---
+++ app/src/Header.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Header.css";
+
+class Header extends Component {
+  render() {
+    return <p>this works!</p>;
+  }
+}
+
+export default Header;
