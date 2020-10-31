b523cbb47c1893ff4fcba2d1edd1b6ef574f125e
Jon George <23463258+JonGeorge@users.noreply.github.com>
2020-10-30 20:43:50 -0400

    Add reactstrap and begin header

--- app/src/Header.js
+++ app/src/Header.js
@@ -1,9 +1,59 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
+import {
+  Collapse,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+} from "reactstrap";
 import "./Header.css";
 
 class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false,
+    };
+  }
+
   render() {
-    return <p>this works!</p>;
+    const toggle = () => this.setState({ isOpen: !this.state.isOpen });
+
+    return (
+      <div>
+        <Navbar className="header" expand="md">
+          <NavbarBrand href="#" className="title">
+            Jon George
+          </NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav navbar>
+              <NavItem>
+                <NavLink href="#">about</NavLink>
+              </NavItem>
+
+              <NavItem>
+                <NavLink href="#">projects</NavLink>
+              </NavItem>
+
+              <NavItem>
+                <NavLink href="#">resume</NavLink>
+              </NavItem>
+
+              <NavItem>
+                <NavLink href="#">articles</NavLink>
+              </NavItem>
+
+              <NavItem>
+                <NavLink href="#">contact</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
   }
 }
 
