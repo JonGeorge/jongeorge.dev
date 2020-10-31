a16e748f9283809664a6ff40ecad064fc77f0376
Jon George <23463258+JonGeorge@users.noreply.github.com>
2020-10-31 13:39:14 -0400

    Add markup and styling for landing page

---
+++ app/src/About.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import { Container, Row, Col } from "reactstrap";
+
+// import "./About.scss";
+
+class About extends Component {
+  render() {
+    return (
+      <Container className="full-height">
+        <Row className="center">
+          <Col xs="12" className="ft-size-14 pad">
+            Workflow ecosystem I have zero cycles for this. Pre launch high
+            touch client nor optics or drink the Kool-aid, but drill down. Put
+            your feelers out we’re starting to formalize flexible opinions
+            around our foundations so low engagement we need a recap by eod, cob
+            or whatever comes first or push back. Put your feelers out build on
+            a culture of contribution and inclusion nor gain traction. Drive
+            awareness to increase engagement manage expectations so flesh that
+            out yet move the needle product management breakout fastworks
+            thought shower pro-sumer software.
+          </Col>
+        </Row>
+      </Container>
+    );
+  }
+}
+
+export default About;
--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import Header from "./Header";
 import "./App.css";
+import Header from "./Header";
+import About from "./About";
 
 function App() {
   return (
     <div class="App">
       <Header />
+      <About />
     </div>
   );
 }
