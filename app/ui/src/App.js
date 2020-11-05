cad1e74992a157e262c0c61343d83b43011b763d
Jon George <23463258+JonGeorge@users.noreply.github.com>
2020-11-05 18:30:59 -0500

    Change App to class component

--- app/ui/src/App.js
+++ app/ui/src/App.js
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "./Header";
 import About from "./About";
 import Projects from "./Projects";
-import Resume from "./Resume";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <About />
-      <Projects />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <About />
+        <Projects />
+      </div>
+    );
+  }
 }
 
 export default App;
