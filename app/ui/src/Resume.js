ca886ea00ebc4fbd5a77d4ef6d01c065d0f718f2
Jon George <23463258+JonGeorge@users.noreply.github.com>
2020-11-11 13:50:13 -0500

    Add Timeline component

--- app/ui/src/Resume.js
+++ app/ui/src/Resume.js
@@ -1,13 +1,29 @@
 import React, { Component } from "react";
 import Timeline from "./Timeline";
 
+import placeholder from "./placeholder.json";
+
 class Resume extends Component {
+constructor() {
+  super();
+  this.state = {
+    currentEntry: 3
+  };
+}
+
+handleClick = (currentEntry) => {
+  this.setState({currentEntry});
+
+}
+
   render() {
+    const entries = placeholder.timelineEntries;
+
     return (
-      <div className="timeline">
-        <center>
-          <Timeline />
-        </center>
+      <div>
+      
+          <Timeline entries={entries} current={this.state.currentEntry} onClick={this.handleClick}/>
+
       </div>);
   }
 }
--- app/ui/src/Timeline.js
+++ app/ui/src/Timeline.js
@@ -2,7 +2,27 @@ import React, { Component } from "react";
 
 class Timeline extends Component {
   render() {
-    return <div className="line">&nbsp;</div>;
+
+    return (
+      <div className="timeline">
+        {this.props.entries.map(entry => {
+          const classes = this.props.current === entry.index ? "entry active" : "entry";
+
+          if (this.props.current === entry.index) {
+            return (
+              <div className={classes} key={entry.index}>
+                <div className="date">{entry.text}</div>
+              </div>
+            );
+          }
+
+          return (
+            <div className={classes} onClick={() => this.props.onClick(entry.index)} key={entry.index}>
+              <div className="hide">{entry.text}</div>
+            </div>
+          );
+        })}
+       </div>);
   }
 }
 
