f74365c201c27c768b8187cbe0e93d5f00f00d16
Jon George <23463258+JonGeorge@users.noreply.github.com>
2024-05-19 16:00:01 -0400

    Simplify navigation with just Projects and About

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,57 +1,46 @@
-<h1 class="title block">Hi I'm Jon!</h1>
+<script>
+
+    export let data;
+
+
+</script>
+
 <div class="content">
-    <p class="block">
-        This site documents the some of the work I've completed and delivered
-        during my career as a Software Engineer.
-    </p>
-    <p class="block">
-        The documentation serves two purposes:
-    </p>
+    <!--{JSON.stringify(data)}-->
 
-    <ol class="block">
-        <li>
-            It provides you with insight on the projects I've worked on,
-            the types of problems I enjoy solving, and outcomes of those solutions.
-        </li>
-        <li>
-            It helps me to reflect on all the work I've done, the amazing
-            teams I've worked with, and the lessons learned along the way.
-        </li>
-    </ol>
+    <h1>Projects</h1>
 
-    <p class="block">
-        My guiding principles for software development are,
-        "focus on outcomes and outputs",
-        "don't try to boil the ocean",
-        and "keep it simple".
-        In a world where we, as technologists and information experts,
-        provide solutions to complex human problems,
-        these principles have provided a solid foundation for decision making,
-        risk analysis, people management, and technical design.
-    </p>
+    {#each data.projectSummaries as project}
+        <div class="card">
+            <header class="card-header is-shadowless">
+                <p class="card-header-title">{project.title}</p>
+            </header>
 
-    <a href="#0">Check out guiding principles </a>
+            <div class="card-content">
+                <div class="content">
+                    {project.description}
+                    <br/>
+                    <br/>
+                    {project.date}
+                </div>
+            </div>
 
-    <div class="field is-grouped mt-6">
-        <p class="control">
-            <a href="https://github.com/JonGeorge">
-                <button class="button is-link">
-                <span class="icon">
-                    <i class="fa-brands fa-github"></i>
-                </span>
-                    <span>GitHub</span></button>
-            </a>
-        </p>
+            <footer class="card-footer">
+                <!--<a href={project.uri} class="card-footer-item">Ask a question</a>-->
+                <!--<a href={project.uri} class="card-footer-item">See the live site</a>-->
+                <a href={project.uri} class="card-footer-item">Read more
+                    <span class="icon">
+                        <i class="fa-solid fa-angle-right"></i>
+                    </span>
+                </a>
+            </footer>
+        </div>
+    {/each}
 
-        <p class="control">
-            <a href="https://www.linkedin.com/in/jon-georgex/">
-                <button class="button is-link">
-                <span class="icon">
-                    <i class="fa-brands fa-linkedin-in"></i>
-                </span>
-                    <span>LinkedIn</span>
-                </button>
-            </a>
-        </p>
-    </div>
 </div>
+
+<style>
+    h1 {
+        margin-bottom: 1.5rem;
+    }
+</style>
