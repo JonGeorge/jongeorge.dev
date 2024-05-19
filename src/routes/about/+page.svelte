13f4bbb3282ff834df475a44b7fa697ec128d50e
Jon George <23463258+JonGeorge@users.noreply.github.com>
2024-05-19 15:59:34 -0400

    Simplify navigation with just Projects and About

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,1 +1,56 @@
-<h1 class="is-size-3-mobile is-size-2-tablet">About</h1>
+<h1 class="title block">Hi I'm Jon!</h1>
+<div class="content">
+    <p class="block">
+        This site is a portfolio of the work I've delivered as a Software Engineer.
+    </p>
+    <p class="block">
+        This project serves two purposes:
+    </p>
+
+    <ol class="block">
+        <li>
+            It provides insight on the projects I've worked on,
+            the types of problems I enjoy solving, and outcomes of those solutions.
+        </li>
+        <li>
+            It helps me to reflect on all the work I've done, the amazing
+            teams I've worked with, and the lessons learned along the way.
+        </li>
+    </ol>
+
+    <p class="block">
+        My guiding principles for software development are,
+        "focus on outcomes and outputs",
+        "don't try to boil the ocean",
+        and "keep it simple".
+        In a world where we, as technologists and information experts,
+        provide solutions to complex human problems,
+        these principles have provided a solid foundation for decision making,
+        risk analysis, people management, and technical design.
+    </p>
+
+<!--    <a href="#0">Check out guiding principles </a>-->
+
+    <div class="field is-grouped mt-6">
+        <p class="control">
+            <a href="https://github.com/JonGeorge">
+                <button class="button is-link">
+                <span class="icon">
+                    <i class="fa-brands fa-github"></i>
+                </span>
+                    <span>GitHub</span></button>
+            </a>
+        </p>
+
+        <p class="control">
+            <a href="https://www.linkedin.com/in/jon-georgex/">
+                <button class="button is-link">
+                <span class="icon">
+                    <i class="fa-brands fa-linkedin-in"></i>
+                </span>
+                    <span>LinkedIn</span>
+                </button>
+            </a>
+        </p>
+    </div>
+</div>
