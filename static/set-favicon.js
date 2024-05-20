206791cb6f5a787aa6c4b33ac350d727e5c4d0c0
Jon George <23463258+JonGeorge@users.noreply.github.com>
2024-05-20 15:11:04 -0400

    Set favicon based on browser theme (light or dark)

---
+++ static/set-favicon.js
@@ -0,0 +1,16 @@
+const lightThemeFavicon = document.querySelector("link#light-theme-favicon");
+const darkThemeFavicon = document.querySelector("link#dark-theme-favicon");
+
+const matcher = window.matchMedia('(prefers-color-scheme: light)');
+matcher.addEventListener('change', setFavicon);
+
+function setFavicon(){
+    if (matcher.matches) {
+        darkThemeFavicon.remove();
+        document.head.append(lightThemeFavicon);
+    }
+    else {
+        lightThemeFavicon.remove();
+        document.head.append(darkThemeFavicon);
+    }
+}
