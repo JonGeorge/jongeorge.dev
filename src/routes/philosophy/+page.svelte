0d38c8138012715f6a87895c3ee234585b517b51
Jon George <23463258+JonGeorge@users.noreply.github.com>
2024-05-20 18:29:05 -0400

    Move old homepage content to philosophy page

---
+++ src/routes/philosophy/+page.svelte
@@ -0,0 +1,10 @@
+<p class="block">
+    The guiding principles for my work are,
+    "focus on outcomes and outputs",
+    "don't try to boil the ocean",
+    and "keep it simple".
+    In a world where we, as technologists and information experts,
+    provide solutions to complex human problems,
+    these principles provide a solid foundation for decision making,
+    risk analysis, people management, and technical design.
+</p>
