b71c67f266c7096a37b1ff480d988bec3f1a05b9
Jon George <23463258+JonGeorge@users.noreply.github.com>
2024-05-20 17:58:07 -0400

    Update the home page content and layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,47 @@
 <script>
-
     export let data;
-
-
 </script>
 
+<h1 class="title block">Hi, I'm Jon!</h1>
 <div class="content">
-    <!--{JSON.stringify(data)}-->
+    <p class="block">
+        I lead software teams and customers
+        to the successful delivery of their product or service.
+    </p>
+    <p class="block">
+        I can read and write code just as easily as I can create
+        and present a slide deck to leadership. Using creative
+        problem solving, design thinking, risk management, and
+        effective prioritization, I work toward achieving the
+        most critical business outcomes.
+    </p>
 
-    <h1>Projects</h1>
+    <div class="field is-grouped mt-6">
+        <p class="control">
+            <a href="https://github.com/JonGeorge">
+                <button class="button is-link">
+                <span class="icon">
+                    <i class="fa-brands fa-github"></i>
+                </span>
+                    <span>GitHub</span></button>
+            </a>
+        </p>
 
+        <p class="control">
+            <a href="https://www.linkedin.com/in/jon-georgex/">
+                <button class="button is-link">
+                <span class="icon">
+                    <i class="fa-brands fa-linkedin-in"></i>
+                </span>
+                    <span>LinkedIn</span>
+                </button>
+            </a>
+        </p>
+    </div>
+</div>
+
+<!--PROJECTS-->
+<div class="content">
     {#each data.projectSummaries as project}
         <div class="card">
             <header class="card-header is-shadowless">
@@ -40,7 +72,5 @@
 </div>
 
 <style>
-    h1 {
-        margin-bottom: 1.5rem;
-    }
+
 </style>
