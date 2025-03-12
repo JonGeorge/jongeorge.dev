5480e9253c901c678f2d6d1d8938d39e0e605ff5
Jon George <23463258+JonGeorge@users.noreply.github.com>
2025-03-12 13:03:04 -0400

    Update mobile nav link hrefs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -121,13 +121,13 @@ export function Header() {
                                                     className="absolute inset-x-0 top-0 z-0 origin-top rounded-b-2xl bg-gray-50 px-6 pt-32 pb-6 shadow-2xl shadow-gray-900/20"
                                                 >
                                                     <div className="space-y-4">
-                                                        <MobileNavLink href="/#features">
+                                                        <MobileNavLink href="/#about">
                                                             About
                                                         </MobileNavLink>
-                                                        <MobileNavLink href="/#reviews">
+                                                        <MobileNavLink href="/#accomplishments">
                                                             Impact
                                                         </MobileNavLink>
-                                                        <MobileNavLink href="/#pricing">
+                                                        <MobileNavLink href="/#my_role">
                                                             My Role
                                                         </MobileNavLink>
                                                     </div>
