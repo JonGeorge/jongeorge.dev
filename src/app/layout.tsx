b0ec41cd017f32cb08c2958900d6ff40b82636c6
Jon George <23463258+JonGeorge@users.noreply.github.com>
2026-02-16 20:50:52 -0500

    Update indieweb.social to social.jg.dev

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -83,7 +83,7 @@ export const metadata: Metadata = {
     publisher: 'Jon George',
 
     other: {
-        "fediverse:creator": "@jg@indieweb.social",
+        "fediverse:creator": "@jg@jg.dev",
     }
 };
 
@@ -96,7 +96,7 @@ export default function RootLayout({
         <html lang="en" suppressHydrationWarning>
         <head>
             {/*{Mastodon Verification: When I link to jg.dev from my Mastodon profile, they will check that the jg.dev links back to my profile and show a visual indicator on it.}*/}
-            <link rel="me" href="https://indieweb.social/@jg" />
+            <link rel="me" href="https://social.dev.jg/@jg" />
         </head>
         <body
             className={`${lora.variable} ${dmSans.variable} ${jetBrainsMono.variable} font-sans antialiased bg-(--color-bg) text-(--color-text-primary) transition-colors duration-300`}
